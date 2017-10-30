f807bc448662b54f0036b71b159498b2be1ed3f0
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-10-30 12:56:58 +0300

    [CHORE] Create store for all state

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import { persistStore, autoRehydrate } from "redux-persist";
+import thunk from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+import rootReducer from "../reducers/index";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, promiseMiddleware(), createLogger()),
+  autoRehydrate()
+);
+persistStore(store);
+export default store;
