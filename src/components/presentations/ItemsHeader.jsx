b44660989a2053443e2f617ed3ad38ff3199b7a7
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-10-30 12:43:21 +0300

    [FEATURE] Add Item Header presentations component

---
+++ src/components/presentations/ItemsHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemsHeader = () => {
+  return (
+    <div className="item-header">
+      <h4>Title</h4>
+      <h5>description</h5>
+    </div>
+  );
+}
+
+export default ItemsHeader;
