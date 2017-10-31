1e321dc79578ec242c65f818c4b22b7b61e42876
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-10-31 16:42:13 +0300

    [FEATURE] Add stata search state

--- src/components/containers/LandingPage.js
+++ src/components/containers/LandingPage.js
@@ -3,7 +3,9 @@ import { connect } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Navigation } from "../presentations/Navigation";
 import * as actions from "../../actions/UserActions";
+import {searchBucket, searchChange} from "../../actions/bucketListActions";
 import Footer from "../presentations/Footer";
+import { Redirect } from 'react-router';
 import { WelcomeText } from "../presentations/WelcomeText";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
@@ -13,11 +15,29 @@ import DashBoard from "./DashBoard";
 class LandingPage extends Component{
   constructor(props) {
     super(props);
+    this.state = {
+        search: ""
+      }
+    }
+
+  onSearchChange = (e) => {
+    e.preventDefault();
+    let search_value = e.target.value;
+    if (search_value){
+      this.setState({search: search_value.substr(0, 20)})
+      this.props.searchBucket(e.target.value);
+    }
+    this.props.searchChange();
   }
 
   handleLogOut = (e) => {
     this.props.logoutUser();
     localStorage.clear()
+    return (
+
+      <Redirect to="/LoginPage" />
+    )
+
   };
 
   render() {
@@ -26,6 +46,8 @@ class LandingPage extends Component{
         <Router>
           <div>
             <Navigation 
+              search={this.state.search}
+              onSearchChange={this.onSearchChange}
               isLoggedIn={this.props.userDetails.isLoggedIn}
               handleLogOut={this.handleLogOut}
             />
@@ -43,13 +65,16 @@ class LandingPage extends Component{
 
 const mapStateToProps = (state) => {
   return {
-    userDetails: state.auth
+    userDetails: state.auth,
+    bucketsData: state.buckets
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    logoutUser: () => dispatch(actions.logoutUser())
+    logoutUser: () => dispatch(actions.logoutUser()),
+    searchBucket: (searchData) => dispatch(searchBucket(searchData)),
+    searchChange: () => dispatch(searchChange())
   };
 };
 
