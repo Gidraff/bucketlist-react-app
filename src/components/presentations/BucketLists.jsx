dadf76df0b41bf6aa7d45e5a974ad465e6e4fe55
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-10-31 16:43:59 +0300

    [FEATURE]  Add list for searched Items

--- src/components/presentations/BucketLists.jsx
+++ src/components/presentations/BucketLists.jsx
@@ -4,12 +4,22 @@ import Bucket from "./Bucket";
 
 const BucketList = (props) => {
   const { bucketlists } = props.bucketlistsData;
+  const { searchBuckets } = props.bucketlistsData;
 
   const listItems = bucketlists.map(bucket => (
     <div className="list-item" key={bucket.id}>
       <a href="#" onClick={props.showItems(bucket.id)} > {bucket.title} </a>
       <button className="form-button" onClick={props.handleClick(bucket.id)}>Edit</button>
       <button className="form-button" onClick={props.handleDeleteClick(bucket.id)}>Delete</button>
+    </div>
+  ));
+
+  const searchItems = searchBuckets.map(bucket => (
+    <div className="list-item" key={bucket.id}>
+      <a href="#" onClick={props.showItems(bucket.id)} >{bucket.title}</a>
+      <button className="form-button" onClick={props.handleClick(bucket.id)}>Edit</button>
+      <button className="form-button" onClick={props.handleDeleteClick(bucket.id)}>Delete</button>
+
     </div>
   ));
 
@@ -24,7 +34,7 @@ const BucketList = (props) => {
   return (
     <div className="list-container">
       <h4 className="header-title">Bucket Lists</h4>
-      <Bucket listItems={listItems} />
+      <Bucket listItems={props.bucketlistsData.isSearch ? searchItems : listItems } />
     </div>
 
   );
