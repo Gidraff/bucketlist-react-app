f33ba099234c58ddda5935609ab141b336e16891
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-06 16:02:26 +0300

    [FIX] Change double quotes to single quotes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import LandingPage from "./components/containers/LandingPage";
-import store from "./store/configureStore";
-
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import store from './store/configureStore';
 
 render(
   <MuiThemeProvider>
     <Provider store={store}>
-      <LandingPage />
+      <Routes />
     </Provider>
   </MuiThemeProvider> , document.getElementById("root"));
