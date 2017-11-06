225136d5c3ad378085307c26773f6bb2810f7427
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-06 16:10:40 +0300

    [FIX] Change Header from Create A Bucket List To Add Bucket

--- src/components/presentations/BucketListForm.jsx
+++ src/components/presentations/BucketListForm.jsx
@@ -4,7 +4,7 @@ import { Form, FormGroup, Label, Input } from "reactstrap";
 export const BucketListForm = props => (
   <div className="buck-form-container">
     <Form className="create-bucket-form" onSubmit={props.onSubmit}>
-      <h4>Create A Bucket List:</h4>
+      <h4>Add Bucket:</h4>
       <FormGroup >
         <Label for="title">Title</Label>
         <Input
