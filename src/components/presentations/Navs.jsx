b19db09afd320b1697f0510f2d9c9a6bb6d6233f
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-06 16:14:02 +0300

    [CHORE] Add a Nav file that contains all navigation

---
+++ src/components/presentations/Navs.jsx
@@ -0,0 +1,24 @@
+import React from  "react";
+import { Link } from "react-router-dom";
+
+export const AuthenticatedNav = (props) => {
+  return (
+    <nav className="link-containers clearfix" >
+      <ul className="navigation-list">
+        <li><input type="text" placeholder="Search..."/></li>
+        <li><Link  onClick={props.handleLogout}to="/login">Logout</Link></li>
+      </ul>
+    </nav>
+  );
+};
+  
+export const UnAuthenticatedNav = () => {
+  return (
+    <nav className="link-containers clearfix" >
+      <ul className="navigation-list">
+        <li><Link to="/">Register</Link></li>
+        <li ><Link  to="/login" >Login</Link></li>
+      </ul>
+    </nav>
+  );
+};
