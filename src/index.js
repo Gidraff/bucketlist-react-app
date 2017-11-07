40c54aabca7a37e991ab08ca82ba27b0cf9011d2
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-07 19:08:25 +0300

    [FIX] Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
 import Routes from './routes';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
