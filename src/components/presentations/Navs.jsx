84388cc15f9e05104a0b065cc4ad9895c93c9f7a
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-07 19:18:18 +0300

    [FEATURE] Add inpur field for search box

--- src/components/presentations/Navs.jsx
+++ src/components/presentations/Navs.jsx
@@ -1,11 +1,16 @@
-import React from  "react";
-import { Link } from "react-router-dom";
+import React from  'react';
+import { Link } from 'react-router-dom';
 
 export const AuthenticatedNav = (props) => {
   return (
     <nav className="link-containers clearfix" >
       <ul className="navigation-list">
-        <li><input type="text" placeholder="Search..."/></li>
+        <li>
+          <input
+            type="text"
+            onChange={props.bucketsData.isSearchItem ?
+              props.onSearchItemChange : props.onSearchBucketChange}
+            className="search-bar" placeholder="Search..."/></li>
         <li><Link  onClick={props.handleLogout}to="/login">Logout</Link></li>
       </ul>
     </nav>
