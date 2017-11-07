4d101e0df4479806a03f1d524162407a3c661aaf
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-08 01:25:48 +0300

    [FIX] Change double quotes to single quotes

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { render } from "react-dom";
-import { Switch, BrowserRouter, Route, Redirect, Link } from "react-router-dom";
-import RegisterContainer from "./components/containers/RegisterContainer";
-import LoginContainer from "./components/containers/LoginContainer";
-import DashBoard from "./components/containers/DashBoard";
-import Navigation from "./components/containers/Navigation";
-import Footer from "./components/presentations/Footer";
-import LoginForm from "./components/presentations/LoginForm";
-import PrivateRoute from "./components/containers/PrivateRoute";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import RegisterContainer from './components/containers/RegisterContainer';
+import LoginContainer from './components/containers/LoginContainer';
+import DashBoard from './components/containers/DashBoard';
+import Navigation from './components/containers/Navigation';
+import Footer from './components/presentations/Footer';
+import PrivateRoute from './components/containers/PrivateRoute';
 
 const Routes =  () => (
   <BrowserRouter>
     <div>
       <Navigation />
-      <Route exact path="/" component={RegisterContainer} />
-      <Route path="/login" component={LoginContainer} />
-      <PrivateRoute  path="/dashboard" component={DashBoard}/>
+      <Route exact path='/' component={RegisterContainer} />
+      <Route path='/login' component={LoginContainer} />
+      <PrivateRoute  path='/dashboard' component={DashBoard}/>
       <Footer />
     </div>
   </BrowserRouter>
