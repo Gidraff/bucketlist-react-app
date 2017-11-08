15677f8829ba935866b5ef5f070f4a05df335741
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-08 15:02:27 +0300

    [FEATURE] Add Component to persist store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import store from './store/configureStore';
+import { persistStore } from 'redux-persist';
+
+export default class AppProvider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rehydrated: false
+    };
+  }
+
+  componentWillMount() {
+    persistStore(store, {}, () => {
+      this.setState({
+        rehydrated: true
+      });
+    });
+  }
+
+  render() {
+    if (!this.state.rehydrated) {
+      return <div> Loading... </div>;
+    }
+    return (
+      <MuiThemeProvider>
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
+
+}
 
 render(
-  <MuiThemeProvider>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </MuiThemeProvider> , document.getElementById('root'));
+  <AppProvider />, document.getElementById('root')
+);
+
+
+
