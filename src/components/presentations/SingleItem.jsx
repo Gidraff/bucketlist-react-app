b48c55cdff34892c1edc80dfd579c5f1750694ab
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-10 10:27:31 +0300

    [FEATURE] Add a Span tag to contain title and description

--- src/components/presentations/SingleItem.jsx
+++ src/components/presentations/SingleItem.jsx
@@ -4,11 +4,15 @@ const SingleItem = (props) => {
   return (
     <div>
       <span>
-        <span className='items-top-header'>Items</span>
+        <span className='items-top-header'>
+          {props.items.length < 1 ? 'No Items available' : <p><b>{props.bucketListData.currentBucketTitle}</b></p>}
+          {props.bucketListData.currentBucketDescription ? 
+            <p>{props.bucketListData.currentBucketDescription}</p> : <p className="description">No description</p> }
+        </span>
         <button
           onClick={props.handleClick(props.id)} 
-          className='pull-right btn-success add-button' href=''>Add Item</button>
-        <button onClick={props.hideItems} className='pull-right back-button'>Go Back</button>
+          className='pull-right btn-primary add-button' href=''>Add Item</button>
+        <button onClick={props.hideItems} className='pull-right  btn-default back-button'>Go Back</button>
       </span>
       <div>{props.bucketItems}</div>
     </div>
