e0f1f29912ccc4823e646a2f2801029aac071680
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-10 10:24:00 +0300

    [FEATURE] Add  a function to handle pagination

--- src/components/presentations/BucketLists.jsx
+++ src/components/presentations/BucketLists.jsx
@@ -9,7 +9,7 @@ const BucketList = (props) => {
 
   const listItems = bucketlists.map(bucket => (
     <div className="list-item" key={bucket.id}>
-      <a href="#" onClick={props.showItems(bucket.id)} > {bucket.title} </a>
+      <a href="#" onClick={props.showItems({ id :bucket.id, title:bucket.title})} > {bucket.title} </a>
       <Glyphicon glyph="trash" className="form-button" onClick={props.handleDeleteClick(bucket.id)} />
       <Glyphicon glyph="pencil" className="form-button" onClick={props.handleClick(bucket.id)} />
     </div>
@@ -17,7 +17,7 @@ const BucketList = (props) => {
 
   const searchItems = searchBuckets && searchBuckets.length >= 1  ?  searchBuckets.map(bucket => (
     <div className="list-item" key={bucket.id}>
-      <a href="#" onClick={props.showItems(bucket.id)} >{bucket.title}</a>
+      <a href="#" onClick={props.showItems({ id :bucket.id, title:bucket.title})} >{bucket.title}</a>
       <Glyphicon  glyph="trash" className="form-button" onClick={props.handleDeleteClick(bucket.id)} />
       <Glyphicon  glyph="pencil" className="form-button" onClick={props.handleClick(bucket.id)} />
     </div>
@@ -30,9 +30,42 @@ const BucketList = (props) => {
       </div>
     );
   }
+
+  const handlePages = (url, e) => {
+    e.preventDefault();
+    props.getBucketsPages(url);
+  };
+
   return (
     <div className="list-container">
-      <h6 className="header-title">Bucket Lists</h6>
+      <h6 className="header-title">Bucket Lists
+        {props.bucketlistsData.pages > 1
+          ?
+          <span className="pages-buttons clearfix">
+            <button 
+              className={props.bucketlistsData.prevPage ? 'prev-page' : 'no-prev-page'}
+              onClick={(e) => handlePages(props.bucketlistsData.prevPage, e)}
+              disabled={
+                props.bucketlistsData.prevPage.length <= 1 
+                  ?
+                  true
+                  :
+                  false}
+            >Prev</button>
+            <button
+              className={props.bucketlistsData.nextPage ? 'next-page' : 'no-next-page'}
+              onClick={(e) => handlePages(props.bucketlistsData.nextPage, e)}
+              disabled={
+                props.bucketlistsData.nextPage.length <= 1 
+                  ?
+                  true
+                  :
+                  false}
+            >Next</button>
+          </span>
+          :
+          null}
+      </h6>
       <Bucket 
         listItems={props.bucketlistsData.isSearch ? searchItems : listItems } />
     </div>
