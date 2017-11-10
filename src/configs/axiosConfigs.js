23cef2951b3eecdad307ce347589cb53e7790627
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-10 10:28:30 +0300

    [FIX] Fix eslint error by change double quotes to single quotes

--- src/configs/axiosConfigs.js
+++ src/configs/axiosConfigs.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-import store from "../store/configureStore";
+import axios from 'axios';
+import store from '../store/configureStore';
 
-const url = "http://127.0.0.1:5000/";
+const url = 'http://127.0.0.1:5000/';
 
 const instance = axios.create({
   baseURL: url,
   timeout: 5000,
   headers: {
-    Accept: "application/json",
-    ContentType: "application/json",
+    Accept: 'application/json',
+    ContentType: 'application/json',
   },
 });
 
@@ -17,7 +17,7 @@ instance.interceptors.request.use((config) => {
   if (token) {
     config.headers.authorization = `${token}`;
   }
-  config.headers["Access-Control-Allow-Origin"] = "*";
+  config.headers['Access-Control-Allow-Origin'] = '*';
   return config;
 });
 
