ffc5dae96b5d79b6d1f731a4c7da6ca8d21da32e
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-14 12:26:29 +0300

    [CHORE] Remove  extra white spaces

--- src/components/presentations/SingleItem.jsx
+++ src/components/presentations/SingleItem.jsx
@@ -6,11 +6,11 @@ const SingleItem = (props) => {
       <span>
         <span className='items-top-header'>
           {props.items.length < 1 ? 'No Items available' : <p><b>{props.bucketListData.currentBucketTitle}</b></p>}
-          {props.bucketListData.currentBucketDescription ? 
+          {props.bucketListData.currentBucketDescription ?
             <p>{props.bucketListData.currentBucketDescription}</p> : <p className="description">No description</p> }
         </span>
         <button
-          onClick={props.handleClick(props.id)} 
+          onClick={props.handleClick(props.id)}
           className='pull-right btn-primary add-button' href=''>Add Item</button>
         <button onClick={props.hideItems} className='pull-right  btn-default back-button'>Go Back</button>
       </span>
