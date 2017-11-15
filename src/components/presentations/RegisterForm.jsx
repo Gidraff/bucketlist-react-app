e98a2764979fefaec56c84a941ce7094cbdb524b
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-15 13:12:02 +0300

    [CHORE] Add regular expression to input field

--- src/components/presentations/RegisterForm.jsx
+++ src/components/presentations/RegisterForm.jsx
@@ -6,10 +6,10 @@ const RegisterForm = props => (
     <Form className="form-container" onSubmit={props.onSignUpSubmit}>
       <FormGroup className="form">
         <h5 className="warning-header">{props.error}</h5>
-        <p><b>Enter you Credentials to Register</b></p>
+        <p className="register-header-text"><b>Fill in the form to create your account</b></p>
         <Label for="exampleEmail" className="lab">Username</Label>
         <Input
-          pattern=".{3,}"
+          pattern="[a-z]{3,}"
           required
           type="text"
           onChange={props.onSignUpChange}
@@ -17,6 +17,7 @@ const RegisterForm = props => (
           className="username"
           value={props.signUpData.username}
           placeholder="Enter you username"
+          title="Invalid username eg example john"
         />
         <Label for="email" className="lab">Email</Label>
         <Input
@@ -27,11 +28,12 @@ const RegisterForm = props => (
           className="username"
           value={props.signUpData.email}
           placeholder="Enter you email"
+          title="Invalid email, john@doe.com"
         />
         <Label for="password" className="lab">password</Label>
         <Input
           pattern=".{7,20}"
-          required
+          min="8"
           type="password"
           onChange={props.onSignUpChange}
           name="password"
@@ -42,7 +44,7 @@ const RegisterForm = props => (
         <Label for="password" className="lab">Confirm</Label>
         <Input
           pattern=".{7,20}"
-          required
+          min="8"
           type="password"
           onChange={props.onSignUpChange}
           name="confirm"
