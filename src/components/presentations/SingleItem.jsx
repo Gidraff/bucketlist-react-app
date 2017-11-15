178d4709345b07776dc2d72b85d410b342cfee3a
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-11-15 13:12:32 +0300

    [CHORE] Add state to component

--- src/components/presentations/SingleItem.jsx
+++ src/components/presentations/SingleItem.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SingleItem = (props) => {
   return (
     <div>
       <span>
         <span className='items-top-header'>
-          {props.items.length < 1 ? 'No Items available' : <p><b>{props.bucketListData.currentBucketTitle}</b></p>}
+          {props.bucketListData.currentBucketTitle} <span className="item-size">{props.items.length}</span><br />
+          {props.items.length < 1 ? 'No Items available' : props.items.length + ' items'}
           {props.bucketListData.currentBucketDescription ?
             <p>{props.bucketListData.currentBucketDescription}</p> : <p className="description">No description</p> }
         </span>
         <button
-          onClick={props.handleClick(props.id)}
+          onClick={props.handleClick({
+            id: props.bucketItemData.current_id,
+            title: props.bucketItemData.currentBucketTitle,
+            description: props.bucketItemData.currentBucketDescription
+          })}
           className='pull-right btn-primary add-button' href=''>Add Item</button>
         <button onClick={props.hideItems} className='pull-right  btn-default back-button'>Go Back</button>
       </span>
@@ -19,4 +25,10 @@ const SingleItem = (props) => {
   );
 };
 
-export default SingleItem;
+const mapStateToProps = state => {
+  return{
+    bucketItemData: state.buckets
+  };
+};
+
+export default connect(mapStateToProps)(SingleItem);
